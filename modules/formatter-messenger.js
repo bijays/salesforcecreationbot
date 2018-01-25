18315ea8358307c33b3c5ba45fc126becbc5c792
Bijay Kumar Saha <bijay.rohit@gmail.com>
2018-01-25 11:39:48 +0000

    revert

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -206,7 +206,7 @@ let formatOpportunities = opportunities => {
     };
 };
 
-exports.org = org;
+//exports.org = org;
 exports.formatBlocks = formatBlocks;
 //exports.formatSubBlocks = formatSubBlocks;
 exports.formatAccounts = formatAccounts;
