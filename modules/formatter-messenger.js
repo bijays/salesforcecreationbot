a1ca0b37a7a86e78e7f44c6f470f0f95e12d5c22
Bijay Kumar Saha <bijay.rohit@gmail.com>
2018-01-26 09:18:06 +0000

    added logic in wbhooks

--- modules/formatter-messenger.js
+++ modules/formatter-messenger.js
@@ -48,7 +48,7 @@ function sfConnection() {
 
 let formatBlocks = blocks => {
     let elements = [];
-    //sfConnection();
+    sfConnection();
     //console.log(response);
     blocks.forEach(block =>
         elements.push({
@@ -207,7 +207,7 @@ let formatOpportunities = opportunities => {
     };
 };
 
-//exports.org = org;
+exports.org = org;
 exports.formatBlocks = formatBlocks;
 //exports.formatSubBlocks = formatSubBlocks;
 exports.formatAccounts = formatAccounts;
